da6b38e98e72acc97a8040fe6c8b68ce76aa9221
jahid <40070056+jahid1o@users.noreply.github.com>
2023-11-11 14:41:07 +0600

    added operate, AC and equals functionality

--- js/script.js
+++ js/script.js
@@ -14,9 +14,15 @@ function divide(num1, num2) {
     return num1 / num2
 }
 
+function remainder(num1, num2) {
+    return num1 % num2
+}
+
 let operator, number1, number2
 
 function operate(opt, n1, n2) {
+    n1 = Number(n1)
+    n2 = Number(n2)
     switch (opt) {
         case "+":
             return add(n1, n2)
@@ -26,7 +32,13 @@ function operate(opt, n1, n2) {
             return multiply(n1, n2)
         case "÷":
             return divide(n1, n2)
+        case "%":
+            return remainder(n1, n2)
     }
+}
+
+function del() {
+    //
 }
 
 const value = document.querySelector("#input")
@@ -53,6 +65,37 @@ numbers.forEach(number => {
         } else {
             number2 += event.target.textContent
             value.value += event.target.textContent
+        }
+    })
+})
+
+operators.forEach(opt => {
+    opt.addEventListener("click", event => {
+        if (number2 != undefined) {
+            number1 = operate(operator, number1, number2)
+            number2 = undefined
+            operator = event.target.textContent
+            value.value = number1 + operator
+        } else if (number1 != undefined) {
+            operator = event.target.textContent
+            value.value = number1 + operator
+        }
+    })
+})
+
+functions.forEach(func => {
+    func.addEventListener("click", event => {
+        switch (event.target.textContent) {
+            case "AC":
+                number1 = number2 = operator = undefined
+                value.value = "0"
+                break
+            case "=":
+                if (number2 != undefined) {
+                    number1 = operate(operator, number1, number2)
+                    number2 = operator = undefined
+                    value.value = number1
+                }
         }
     })
 })
