a3ca8517391f8d627f814e93a44fed5f705ba0bc
jahid <40070056+jahid1o@users.noreply.github.com>
2023-11-11 13:53:20 +0600

    added operate function

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2
 }
+
+let operator, number1, number2
+
+function operate(opt, n1, n2) {
+    switch (opt) {
+        case "+":
+            return add(n1, n2)
+        case "-":
+            return subtract(n1, n2)
+        case "×":
+            return multiply(n1, n2)
+        case "÷":
+            return divide(n1, n2)
+    }
+}
